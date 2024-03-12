6c962d7d34ce42898d71dc8959473e3067f7d4e4
sshahirr <126746195+sshahirr@users.noreply.github.com>
2024-03-12 19:41:47 -0400

    Trending.js

--- Movie.js
+++ Movie.js
@@ -15,12 +15,35 @@ function Movie() {
 
     console.log(movieList)
 
+    const handleMouseEnter = (index) => {
+      const image = document.getElementById(`image_${index}`);
+      if (image) {
+        image.style.transform = "scale(1.1)"; // Enlarge by 10%
+      }
+    };
+  
+    // Function to handle mouse leave event (restore original size)
+    const handleMouseLeave = (index) => {
+      const image = document.getElementById(`image_${index}`);
+      if (image) {
+        image.style.transform = "scale(1)"; // Restore original size
+      }
+    };
+
     return (
       <div style={{ backgroundColor: '#1E0D1E', padding: '30px' }}>
       <h1 style={{color: 'white', fontSize: '75px'}}>Trending</h1>
-      {movieList.map((movie)=>(
-        <img style={{width:"400px", height:"600px", marginLeft:"10px",marginTop:"10px", borderRadius:'17px'}} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}/>
+      {movieList.map((movie, index)=>(
+        <img 
+          key = {index}
+          id = {`image_${index}`}
+          style={{width:"400px", height:"px", marginLeft:"10px",marginTop:"10px", borderRadius:'17px', transition: `transform 0.3s`}} 
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          onMouseEnter={() => handleMouseEnter(index)}
+          onMouseLeave={() => handleMouseLeave(index)}
+        />
       ))}
+      <p style={{color: 'white'}}>Courtesy of TMDb</p>
     </div>
   )
 }
