2a1305ec7cf3b805044c831f7f6646e9039e0576
hitagisbf <128249730+hitagisbf@users.noreply.github.com>
2024-04-01 21:19:46 -0400

    App.jsx

--- App.jsx
+++ App.jsx
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <div>
       <h1>Filmic Forensics quiz page</h1> 
-      <Quiz /> {/* Render the Quiz component */}
+      <Quiz /> 
     </div>
   );
 };// this is just the header and the Quiz part is useful because it renders the Quiz component within this App.jsx meaning it is rendered without any children
