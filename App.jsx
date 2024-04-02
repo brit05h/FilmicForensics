d758b4e5d1dc0644a56ecb11a22e3b33a602efcb
hitagisbf <128249730+hitagisbf@users.noreply.github.com>
2024-04-01 21:16:43 -0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,17 @@
+// App.jsx
+import React from 'react';
+import Quiz from './Quiz'; // Import the Quiz component
+
+const App = () => {
+  return (
+    <div>
+      <h1>Filmic Forensics quiz page</h1> 
+      <Quiz /> {/* Render the Quiz component */}
+    </div>
+  );
+};// this is just the header and the Quiz part is useful because it renders the Quiz component within this App.jsx meaning it is rendered without any children
+
+export default App;
+
+
+
---
+++ testdorian.js
@@ -0,0 +1,148 @@
+//genre: Action, horrror, etc..
+//actor/actress: Jake G, Anya Taylor Joy, etc...
+//runtime: 90 minutes, blah blah blah 
+
+
+export const movieQuizz = {
+  questions: [
+    {
+      question: "Have you been to the movies more than once?",
+      answers: [
+        {
+          type: "Jake Gyllenhaal, Action,",
+          content: "Of course I have, duh!"
+        },
+        {
+          type: "",
+          content: "Why would you assume I've been to the movies?"
+        },
+        {
+          type: "Mark Ruffalo, Drama,",
+          content: "I've only been there once in my life"
+        }
+      ]
+    },
+    {
+      question: "Do you need something to watch while eating?",
+      answers: [
+        {
+          type: "Scarlett Johansson, Drama,",
+          content: "Yes, all the time please"
+        },
+        {
+          type: "Toni Collette, Horror,",
+          content: "No, I need to focus on my food exclusively"
+        },
+        {
+          type: "",
+          content: "I have no clue, man"
+        }
+      ]
+    },
+    {
+      question: "Do you think you get emotional quickly?",
+      answers: [
+        {
+          type: "Anya Taylor Joy, Romance",
+          content: "My friends tell me all the time."
+        },
+        {
+          type: "",
+          content: "I genuinely don't know."
+        },
+        {
+          type: "Daniel Kaluuya, Horror",
+          content: "I'll shed a tear every now and then"
+        }
+      ]
+    },
+    {
+      question: "Did you use a stick as a wand growing up?",
+      answers: [
+        {
+          type: "Emma Watson, Romance",
+          content: "I hate magic."
+        },
+        {
+          type: "Brad Pitt, Fantasy",
+          content: "I was an actual wizard."
+        },
+        {
+          type: "",
+          content: "What is a wand, what is a stick."
+        }
+      ]
+    },
+    {
+      question: "Do you have siblings?",
+      answers: [
+        {
+          type: "Denzel Washington, Drama",
+          content: "Yes, I love them so much"
+        },
+        {
+          type: "Cilian Murphy, Crime",
+          content: "I wish so bad"
+        },
+        {
+          type: "",
+          content: "I'd rather not answer"
+        }
+      ]
+    },
+    {
+      question: "Have you ever spoken to a cop before",
+      answers: [
+        {
+          type: "Morgan Freeman, Crime",
+          content: "I've had some crazy conversations with a cop before"
+        },
+        {
+          type: "Zendaya, Romance",
+          content: "No I've never needed to"
+        },
+        {
+          type: "",
+          content: "I'd rather not answer this..."
+        }
+      ]
+    },
+    {
+      question: "If you had to pick between a saw, a bike, and a taco what would you choose",
+      answers: [
+        {
+          type: "Morgan Freeman, Horror",
+          content: "A saw, I like saws"
+        },
+        {
+          type: "Anya Taylor Joy, Fantasy",
+          content: "I love biking!"
+        },
+        {
+          type: "Toni Collette, Drama",
+          content: "If you're offering me a taco I will always take it"
+        }
+      ]
+    },
+    {
+      question: "How many tries do you think it would take for you to draw a perfect circle",
+      answers: [
+        {
+          type: "Cilian Murphy, Drama",
+          content: "Less than 50 tries"
+        },
+        {
+          type: "Toni Collette, Action",
+          content: "At least 50 tries all the way up to 10,0000"
+        },
+        {
+          type: "",
+          content: "Can we just go on to the next question"
+        }
+      ]
+    }
+  ]
+};
+
+
+
