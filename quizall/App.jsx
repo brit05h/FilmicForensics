c186f2dabef6f31ca470024f8aa0a8ed2557a1ca
hitagisbf <128249730+hitagisbf@users.noreply.github.com>
2024-04-14 00:38:20 -0400

    Update App.jsx

--- quizall/App.jsx
+++ quizall/App.jsx
@@ -5,8 +5,8 @@ import Quiz from './Quiz'; // Import the Quiz component
 const App = () => {
   return (
     <div>
-      <h1>Filmic Forensics quiz page</h1> 
-      <Quiz />  
+      <h1 className="DR-main-header">Filmic Forensics Quiz Page</h1> 
+      <Quiz /> 
     </div>
   );
 };// this is just the header and the Quiz part is useful because it renders the Quiz component within this App.jsx meaning it is rendered without any children
