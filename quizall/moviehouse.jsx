6135f186b3ba75df97c7a2f24c8cfe2063ed706e
hitagisbf <128249730+hitagisbf@users.noreply.github.com>
2024-04-14 00:39:38 -0400

    Update and rename Question.jsx to moviehouse.jsx

---
+++ quizall/moviehouse.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes for type-checking
+
+const MovieInfo = ({ title, imageUrl, actor, genre }) => {
+  return (
+    <div>
+      <h2>Movie: {title}</h2>
+      <img src={imageUrl} alt={title} style={{ maxWidth: '200px', height: 'auto' }} />
+      <ul>
+        <li><strong>Actor:</strong> {actor}</li>
+        <li><strong>Genre:</strong> {genre}</li>
+      </ul>
+    </div>
+  );
+};
+
+// Define PropTypes for MovieInfo
+MovieInfo.propTypes = {
+  title: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  actor: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired
+};
+
+export default MovieInfo;
+
+
+
+
+
+
