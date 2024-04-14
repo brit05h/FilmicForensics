221925b64d07d1444f5910b26e5e317b5cb3d3bb
hitagisbf <128249730+hitagisbf@users.noreply.github.com>
2024-04-14 00:40:13 -0400

    Update testdorian.js

--- quizall/testdorian.js
+++ quizall/testdorian.js
@@ -1,148 +1,424 @@
-//genre: Action, horrror, etc..
-//actor/actress: Jake G, Anya Taylor Joy, etc...
-//runtime: 90 minutes, blah blah blah 
-
-
 export const movieQuizz = {
   questions: [
     {
-      question: "Have you been to the movies more than once?",
+      question: "Q1/12: How do you like to usually watch Movies?",
       answers: [
         {
-          type: "Jake Gyllenhaal, Action,",
-          content: "Of course I have, duh!"
+          actor: 192,
+          genre: 28,
+          content: "I don't really watch movies"
         },
         {
-          type: "",
-          content: "Why would you assume I've been to the movies?"
+          actor: 3051,
+          genre: 80,
+          content: 'Why would you assume I watch movies?'
         },
         {
-          type: "Mark Ruffalo, Drama,",
-          content: "I've only been there once in my life"
+          actor: 103,
+          genre: 28,
+          content: "I go to the theatres to watch movies"
+        },
+        {
+          actor: 505710,
+          genre: 28,
+          content: 'I try to relax at home and watch at least one movie per week'
+        },
+        {
+          actor: 76788,
+          genre: 53,
+          content: 'I only watch movies with friends'
+        },
+        {
+          actor: 206919,
+          genre: 28,
+          content: 'I just like to watch movies online'
         }
       ]
     },
     {
-      question: "Do you need something to watch while eating?",
+      question: "Q2/12: Would you consider yourself an IPAD kid while eating?",
       answers: [
         {
-          type: "Scarlett Johansson, Drama,",
-          content: "Yes, all the time please"
+          actor: 1245,
+          genre: 28,
+          content: "I would sadly"
         },
         {
-          type: "Toni Collette, Horror,",
-          content: "No, I need to focus on my food exclusively"
+          actor: 10990,
+          genre: 10749,
+          content: "No, I exclusively focus on my food"
         },
         {
-          type: "",
-          content: "I have no clue, man"
+          actor: 1397778,
+          genre: 14,
+          content: "Sometimes I'm an IPAD Kid other times I'm not"
+        },
+        {
+          actor: 131,
+          genre: 10749,
+          content: 'Why would you assume I even own a device to watch videos?'
+        },
+        {
+          actor: 5292,
+          genre: 80,
+          content: "I don't even know what that terms means"
+        },
+        {
+          actor: 287,
+          genre: 80,
+          content: 'Im only an IPAD Kid when the food I have is really good'
         }
       ]
     },
     {
-      question: "Do you think you get emotional quickly?",
+      question: "Q3/12: Pick one fruit to eat for the rest of your life",
       answers: [
         {
-          type: "Anya Taylor Joy, Romance",
-          content: "My friends tell me all the time."
+          actor: 3051,
+          genre: 27,
+          content: "Banana (I'll make an amazing smoothie)."
         },
         {
-          type: "",
-          content: "I genuinely don't know."
+          actor: 192,
+          genre: 14,
+          content: "Mangoes (The best fruit ever)."
         },
         {
-          type: "Daniel Kaluuya, Horror",
-          content: "I'll shed a tear every now and then"
+          actor: 505710,
+          genre: 10749,
+          content: "Strawberries (Strawberries and chocolate will feed my family("
+        },
+        {
+          actor: 76788,
+          genre: 28,
+          content: "Pineapples (pineapple juice, pineapple upside down cake.."
+        },
+        {
+          actor: 206919,
+          genre: 878,
+          content: "Apples, (apple pie, apple juice, apple tarts)"
+        },
+        {
+          actor: 103,
+          genre: 18,
+          content: "Orange (Oranges are Orange and Orange Juice)"
         }
       ]
     },
     {
-      question: "Did you use a stick as a wand growing up?",
+      question: "Q4/12: What was your favorite class in school?",
       answers: [
         {
-          type: "Emma Watson, Romance",
-          content: "I hate magic."
+          actor: 287,
+          genre: 28,
+          content: "I was more of a science person (mitochondria and stuff)"
         },
         {
-          type: "Brad Pitt, Fantasy",
-          content: "I was an actual wizard."
+          actor: 5292,
+          genre: 99,
+          content: "I was big on history (dates and people dying)."
         },
         {
-          type: "",
-          content: "What is a wand, what is a stick."
+          actor: 131,
+          genre: 18,
+          content: "Not the biggest fan of school ever...."
+        },
+        {
+          actor: 1397778,
+          genre: 10751,
+          content: "I enjoyed math alot (the slope of a tangent line and such)"
+        },
+        {
+          actor: 10990,
+          genre: 14,
+          content: "I loved English class (Shakespeare or Fantasy or anything with words)"
+        },
+        {
+          actor: 1245,
+          genre: 10751,
+          content: "Music class was definitely my favorite (do re ma fa sol la ti do!!!)"
         }
       ]
     },
     {
-      question: "Do you have siblings?",
+      question: "Q5/12: If you could use one mode of transportation for the rest of your life what would you choose?",
       answers: [
         {
-          type: "Denzel Washington, Drama",
-          content: "Yes, I love them so much"
+          actor: 192,
+          genre: 80,
+          content: "I'd personally walk anywhere I needed to be"
         },
         {
-          type: "Cilian Murphy, Crime",
-          content: "I wish so bad"
+          actor: 103,
+          genre: 14,
+          content: "I'd take a boat anywhere and everywhere (even on land)"
         },
         {
-          type: "",
-          content: "I'd rather not answer"
+          actor: 505710,
+          genre: 878,
+          content: "I would literally fly everywhere (on a plane of course)"
+        },
+        {
+          actor: 206919,
+          genre: 27,
+          content: "I'll take my reliable car everywhere"
+        },
+        {
+          actor: 3051,
+          genre: 53,
+          content: "There's nothing more thrilling than biking everywhere"
+        },
+        {
+          actor: 76788,
+          genre: 35,
+          content: "I love my trains no matter what"
         }
       ]
     },
     {
-      question: "Have you ever spoken to a cop before",
+      question: "Q6/12: What type of music do you enjoy listening to?",
       answers: [
         {
-          type: "Morgan Freeman, Crime",
-          content: "I've had some crazy conversations with a cop before"
+          actor: 1245,
+          genre: 10749,
+          content: "Rap (Old school rap though none of the new stuff)"
         },
         {
-          type: "Zendaya, Romance",
-          content: "No I've never needed to"
+          actor: 287,
+          genre: 10749,
+          content: "Rap (I'm talking about that new stuff)"
         },
         {
-          type: "",
-          content: "I'd rather not answer this..."
+          actor: 10990,
+          genre: 35,
+          content: "I don't mind listening to whatever"
+        },
+        {
+          actor: 1397778,
+          genre: 27,
+          content: "I love pop (we can include all international forms of pop as well)"
+        },
+        {
+          actor: 131,
+          genre: 80,
+          content: "Country Music will have a special place in my heart..."
+        },
+        {
+          actor: 5292,
+          genre: 28,
+          content: "I don't really like music with lyrics anything instrumental or classic is for me"
         }
       ]
     },
     {
-      question: "If you had to pick between a saw, a bike, and a taco what would you choose",
+      question: "Q7/12: If you were transported to an alien world with only the things in your pockets what would you do ",
       answers: [
         {
-          type: "Morgan Freeman, Horror",
-          content: "A saw, I like saws"
+          actor: 3051,
+          genre: 80,
+          content: "My pockets have everything I'd be fine"
         },
         {
-          type: "Anya Taylor Joy, Fantasy",
-          content: "I love biking!"
+          actor: 505710,
+          genre: 10749,
+          content: "No Matter what I have I'm just gonna drop and cry"
         },
         {
-          type: "Toni Collette, Drama",
-          content: "If you're offering me a taco I will always take it"
+          actor: 103,
+          genre: 18,
+          content: "I have my phone on me, let me call someone and hope they pick up"
+        },
+        {
+          actor: 206919,
+          genre: 878,
+          content: "No matter what I have I will conquer this planet"
+        },
+        {
+          actor: 192,
+          genre: 14,
+          content: "I wonder if I'd read about something like this before"
+        },
+        {
+          actor: 76788,
+          genre: 53,
+          content: "Hope that I am dreaming and pinc myself a 100 times"
         }
       ]
     },
     {
-      question: "How many tries do you think it would take for you to draw a perfect circle",
+      question: "Q8/12: How many tries do you think it would take for you to draw a perfect circle",
       answers: [
         {
-          type: "Cilian Murphy, Drama",
-          content: "Less than 50 tries"
+          actor: 1397778,
+          genre: 10751,
+          content: "1-10 times"
         },
         {
-          type: "Toni Collette, Action",
-          content: "At least 50 tries all the way up to 10,0000"
+          actor: 287,
+          genre: 80,
+          content: "Just one time I'm actually a professional artist"
         },
         {
-          type: "",
-          content: "Can we just go on to the next question"
+          actor: 1245,
+          genre: 28,
+          content: "About 20-30 times"
+        },
+        {
+          actor: 5292,
+          genre: 99,
+          content: "I can't even do this I'm just going to the next question"
+        },
+        {
+          actor: 10990,
+          genre: 14,
+          content: "Give me a 100 times and I'll do it"
+        },
+        {
+          actor: 131,
+          genre: 80,
+          content: "I can't give you a number but I will do it."
+        }
+      ]
+    },
+    {
+      question: "Q9/12: What is your ideal vacation spot",
+      answers: [
+        {
+          actor: 192,
+          genre: 28,
+          content: "I love a nice tropical place"
+        },
+        {
+          actor: 3051,
+          genre: 27,
+          content: "I love going to vacation in a city like area"
+        },
+        {
+          actor: 103,
+          genre: 14,
+          content: "The best vacations are the ones you have at home"
+        },
+        {
+          actor: 76788,
+          genre: 35,
+          content: "I love farm like areas to vacation in"
+        },
+        {
+          actor: 206919,
+          genre: 27,
+          content: "I don't like vacationing just keep me working"
+        },
+        {
+          actor: 505710,
+          genre: 878,
+          content: "I like colder areas to vacation in"
+        }
+      ]
+    },
+    {
+      question: "Q10/12: What month does your birthday fall in",
+      answers: [
+        {
+          actor: 131,
+          genre: 10749,
+          content: "January or February"
+        },
+        {
+          actor: 5292,
+          genre: 28,
+          content: "March or April"
+        },
+        {
+          actor: 287,
+          genre: 80,
+          content: "May or June"
+        },
+        {
+          actor: 1245,
+          genre: 10751,
+          content: "July or August"
+        },
+        {
+          actor: 10990,
+          genre: 35,
+          content: "September or October"
+        },
+        {
+          actor: 1397778,
+          genre: 10751,
+          content: "November and December"
+        }
+      ]
+    },
+    {
+      question: "Q11/12: What is your favorite game console (of all time)?",
+      answers: [
+        {
+          actor: 206919,
+          genre: 28,
+          content: "Been an Xbox Person my whole life"
+        },
+        {
+          actor: 3051,
+          genre: 53,
+          content: "Fell in love with playstation ever since a kid"
+        },
+        {
+          actor: 192,
+          genre: 80,
+          content: "Nothing is better than nintendo"
+        },
+        {
+          actor: 103,
+          genre: 28,
+          content: "I've been gaming on a PC ever since I opened my eyes"
+        },
+        {
+          actor: 505710,
+          genre: 28,
+          content: "My phone or Ipad is my gaming console tbh"
+        },
+        {
+          actor: 76788,
+          genre: 28,
+          content: "I don't game its not for me"
+        }
+      ]
+    },
+    {
+      question: "Q12/12: I know this question is boring but pick your favorite color from this list",
+      answers: [
+        {
+          actor: 1245,
+          genre: 10749,
+          content: "Blue"
+        },
+        {
+          actor: 287,
+          genre: 10749,
+          content: "Orange"
+        },
+        {
+          actor: 10990,
+          genre: 10749,
+          content: "Red"
+        },
+        {
+          actor: 1397778,
+          genre: 27,
+          content: "Green"
+        },
+        {
+          actor: 131,
+          genre: 10749,
+          content: "Yellow"
+        },
+        {
+          actor: 5292,
+          genre: 28,
+          content: "Purple"
         }
       ]
     }
   ]
 };
-
-
-
